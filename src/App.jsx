2e9d7d82cca53acb2de83ea85a03113c1b00c6de
koybigino <koybigino@gmail.com>
2023-06-25 12:26:25 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@material-tailwind/react";
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center text-red-400">Hello world!</h1>
+      <Button>Button</Button>
+    </>
+  );
+}
+
+export default App
